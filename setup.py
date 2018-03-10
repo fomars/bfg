48371abf5334423d673802cd1306a33fa20ffc1c
fomars <fomars@yandex-team.ru>
2018-03-10 16:38:29 +0300

    v0.5.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bfg',
-    version='0.4.1',
+    version='0.5.2',
     description='a load generation tool and framework',
     longer_description='''
 BFG is a modular tool and framework for load generation.
